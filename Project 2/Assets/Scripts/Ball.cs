2fe6b5537fc6f1ebccdfbf1a21c70d033baf7037
Vente1 <michaelmarshall208@hotmail.com>
2017-09-30 15:47:50 +1000

    Movement

---
+++ Project 2/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float initialVelocity = 1f;
+
+    private bool ballInPlay = false;
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(initialVelocity, initialVelocity, 0.0f));
+        }
+    }
+}
--- Project 2/Assets/Scripts/PaddleControl.cs
+++ Project 2/Assets/Scripts/PaddleControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PaddleControl : MonoBehaviour {
 
+    public float paddleSpeed = 1f;
+
+    private Vector3 paddlePosition = new Vector3(0.0f, -9.5f, 0.0f);
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,8 @@ public class PaddleControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float xPos = transform.position.x + Input.GetAxis("Horizontal") * paddleSpeed;
+        paddlePosition = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0.0f);
+        transform.position = paddlePosition;
 	}
 }
