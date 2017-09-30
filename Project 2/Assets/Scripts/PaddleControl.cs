35e18668658e7d1ddb0271aa73d972f529a33568
Vente1 <michaelmarshall208@hotmail.com>
2017-09-30 19:42:19 +1000

    Fixed input settings
    To change them go to Edit > Project Settings > Input !!

--- Project 2/Assets/Scripts/PaddleControl.cs
+++ Project 2/Assets/Scripts/PaddleControl.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PaddleControl : MonoBehaviour {
 
-    public float paddleSpeed = 1000f;
+    public float paddleSpeed = 0.5f;
 
     private Vector3 paddlePosition = new Vector3(0.0f, -9.5f, 0.0f);
 	
 	// Update is called once per frame
 	void Update () {
-        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed * Time.deltaTime);
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
         paddlePosition = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0.0f);
         transform.position = paddlePosition;
 	}
